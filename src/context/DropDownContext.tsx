d244c30366f0226673c0c7c93afe3d6a595ee19b
Guilherme <gui.franchim@gmail.com>
2023-05-23 20:22:23 -0300

    chore/collecting-and-populating-types

--- src/context/DropDownContext.tsx
+++ src/context/DropDownContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { get } from "../api/pokeApi";
 export type DropDownItems = {
   [type: string]: { [item: string]: boolean };
 };
@@ -14,30 +15,12 @@ export type DropDownContextProps = {
   setSelectedExperience: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
-
 type DropDownContextProviderProps = {
   children: React.ReactNode;
 };
 
 const DEFAULT_VALUES = {
-  dropDownItems: {},
-  setDropDownItems: () => [],
-  selectedTypes: [],
-  setSelectedTypes: () => [],
-  selectedAttacks: [],
-  setSelectedAttacks: () => [],
-  selectedExperience: [],
-  setSelectedExperience: () => [],
-} as DropDownContextProps;
-
-export const DropDownContext = createContext<DropDownContextProps>(
-  DEFAULT_VALUES
-);
-
-export const DropDownContextProvider = ({
-  children,
-}: DropDownContextProviderProps) => {
-  const [dropDownItems, setDropDownItems] = useState<DropDownItems>({
+  dropDownItems: {
     Tipo: {
       Fire: false,
       Normal: false,
@@ -54,13 +37,59 @@ export const DropDownContextProvider = ({
       "Medium Experience": false,
       "High Experience": false,
     },
-  });
+  },
+  setDropDownItems: () => [],
+  selectedTypes: [],
+  setSelectedTypes: () => [],
+  selectedAttacks: [],
+  setSelectedAttacks: () => [],
+  selectedExperience: [],
+  setSelectedExperience: () => [],
+} as DropDownContextProps;
+
+export const DropDownContext =
+  createContext<DropDownContextProps>(DEFAULT_VALUES);
+
+export const DropDownContextProvider = ({
+  children,
+}: DropDownContextProviderProps) => {
+  const [dropDownItems, setDropDownItems] = useState<DropDownItems>({});
   const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
   const [selectedAttacks, setSelectedAttacks] = useState<string[]>([]);
   const [selectedExperience, setSelectedExperience] = useState<string[]>([]);
 
+  const fetchTypePokemon = async () => {
+    const { results } = await get("type");
+
+    let dropDownItems = results.reduce((acc: any, type: any) => {
+      acc[type.name] = false;
+      return acc;
+    }, {});
+    
+    setDropDownItems({
+      Tipo: {...dropDownItems},
+      Ataque: {...DEFAULT_VALUES.dropDownItems.Ataque},
+      Experiencia: {...DEFAULT_VALUES.dropDownItems.Experiencia},
+    });
+  };
+
+  useEffect(() => {
+    fetchTypePokemon();
+  });
+
   return (
-    <DropDownContext.Provider value={{ dropDownItems, setDropDownItems, selectedTypes, setSelectedTypes, selectedAttacks, setSelectedAttacks, selectedExperience, setSelectedExperience }}>
+    <DropDownContext.Provider
+      value={{
+        dropDownItems,
+        setDropDownItems,
+        selectedTypes,
+        setSelectedTypes,
+        selectedAttacks,
+        setSelectedAttacks,
+        selectedExperience,
+        setSelectedExperience,
+      }}
+    >
       {children}
     </DropDownContext.Provider>
   );
